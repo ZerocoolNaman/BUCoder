2f1f17c45cfa3b80f136d1175e73ab8c79083d7a
Naman Tiwari <36857967+ZerocoolNaman@users.noreply.github.com>
2020-11-19 16:00:25 +0530

    Add files via upload

--- User.php
+++ User.php
@@ -1,200 +1,135 @@
 <?php
-class Post {
-	private $user_obj;
+class User {
+	private $user;
 	private $con;
 
 	public function __construct($con, $user){
 		$this->con = $con;
-		$this->user_obj = new User($con, $user);
+		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user'");
+		$this->user = mysqli_fetch_array($user_details_query);
 	}
 
-	public function submitPost($body, $user_to) {
-		$body = strip_tags($body); //removes html tags 
-		$body = mysqli_real_escape_string($this->con, $body);
-		$check_empty = preg_replace('/\s+/', '', $body); //Deltes all spaces 
-      
-		if($check_empty != "") {
-
-
-			//Current date and time
-			$date_added = date("Y-m-d H:i:s");
-			//Get username
-			$added_by = $this->user_obj->getUsername();
-
-			//If user is on own profile, user_to is 'none'
-			if($user_to == $added_by) {
-				$user_to = "none";
-			}
-
-			//insert post 
-			$query = mysqli_query($this->con, "INSERT INTO posts (body,added_by,user_to,date_added,user_closed,deleted,likes) VALUES('$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
-			$returned_id = mysqli_insert_id($this->con);
-
-			//Insert notification 
-
-			//Update post count for user 
-			$num_posts = $this->user_obj->getNumPosts();
-			$num_posts++;
-			$update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
-
-		}
+	public function getUsername() {
+		return $this->user['username'];
 	}
 
-	public function loadPostsFriends($data, $limit) {
+	public function getNumberOfFriendRequests() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$username'");
+		return mysqli_num_rows($query);
+	}
 
-		$page = $data['page']; 
-		$userLoggedIn = $this->user_obj->getUsername();
+	public function getNumPosts() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT num_posts FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['num_posts'];
+	}
 
-		if($page == 1) 
-			$start = 0;
+	public function getFirstAndLastName() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT first_name, last_name FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['first_name'] . " " . $row['last_name'];
+	}
+
+	public function getProfilePic() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT profile_pic FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['profile_pic'];
+	}
+
+	public function getFriendArray() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['friend_array'];
+	}
+
+	public function isClosed() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT user_closed FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+
+		if($row['user_closed'] == 'yes')
+			return true;
 		else 
-			$start = ($page - 1) * $limit;
+			return false;
+	}
 
+	public function isFriend($username_to_check) {
+		$usernameComma = "," . $username_to_check . ",";
 
-		$str = ""; //String to return 
-		$data_query = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
-
-		if(mysqli_num_rows($data_query) > 0) {
-
-
-			$num_iterations = 0; //Number of results checked (not necasserily posted)
-			$count = 1;
-
-			while($row = mysqli_fetch_array($data_query)) {
-				$id = $row['id'];
-				$body = $row['body'];
-				$added_by = $row['added_by'];
-				$date_time = $row['date_added'];
-
-				//Prepare user_to string so it can be included even if not posted to a user
-				if($row['user_to'] == "none") {
-					$user_to = "";
-				}
-				else {
-					$user_to_obj = new User($con, $row['user_to']);
-					$user_to_name = $user_to_obj->getFirstAndLastName();
-					$user_to = "to <a href='" . $row['user_to'] ."'>" . $user_to_name . "</a>";
-				}
-
-				//Check if user who posted, has their account closed
-				$added_by_obj = new User($this->con, $added_by);
-				if($added_by_obj->isClosed()) {
-					continue;
-				}
-
-				
-
-					if($num_iterations++ < $start)
-						continue; 
-
-
-					//Once 10 posts have been loaded, break
-					if($count > $limit) {
-						break;
-					}
-					else {
-						$count++;
-					}
-
-					$user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
-					$user_row = mysqli_fetch_array($user_details_query);
-					$first_name = $user_row['first_name'];
-					$last_name = $user_row['last_name'];
-					$profile_pic = $user_row['profile_pic'];
-
-
-					//Timeframe
-					$date_time_now = date("Y-m-d H:i:s");
-					$start_date = new DateTime($date_time); //Time of post
-					$end_date = new DateTime($date_time_now); //Current time
-					$interval = $start_date->diff($end_date); //Difference between dates 
-					if($interval->y >= 1) {
-						if($interval == 1)
-							$time_message = $interval->y . " year ago"; //1 year ago
-						else 
-							$time_message = $interval->y . " years ago"; //1+ year ago
-					}
-					else if ($interval-> m >= 1) {
-						if($interval->d == 0) {
-							$days = " ago";
-						}
-						else if($interval->d == 1) {
-							$days = $interval->d . " day ago";
-						}
-						else {
-							$days = $interval->d . " days ago";
-						}
-
-
-						if($interval->m == 1) {
-							$time_message = $interval->m . " month". $days;
-						}
-						else {
-							$time_message = $interval->m . " months". $days;
-						}
-
-					}
-					else if($interval->d >= 1) {
-						if($interval->d == 1) {
-							$time_message = "Yesterday";
-						}
-						else {
-							$time_message = $interval->d . " days ago";
-						}
-					}
-					else if($interval->h >= 1) {
-						if($interval->h == 1) {
-							$time_message = $interval->h . " hour ago";
-						}
-						else {
-							$time_message = $interval->h . " hours ago";
-						}
-					}
-					else if($interval->i >= 1) {
-						if($interval->i == 1) {
-							$time_message = $interval->i . " minute ago";
-						}
-						else {
-							$time_message = $interval->i . " minutes ago";
-						}
-					}
-					else {
-						if($interval->s < 30) {
-							$time_message = "Just now";
-						}
-						else {
-							$time_message = $interval->s . " seconds ago";
-						}
-					}
-
-					$str .= "<div class='status_post'>
-								<div class='post_profile_pic'>
-									<img src='$profile_pic' width='50'>
-								</div>
-
-								<div class='posted_by' style='color:#ACACAC;'>
-									<a href='$added_by'> $first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
-								</div>
-								<div id='post_body'>
-									$body
-									<br>
-								</div>
-
-							</div>
-							<hr>";
-				
-
-			} //End while loop
-
-			if($count > $limit) 
-				$str .= "<input type='hidden' class='nextPage' value='" . ($page + 1) . "'>
-							<input type='hidden' class='noMorePosts' value='false'>";
-			else 
-				$str .= "<input type='hidden' class='noMorePosts' value='true'><p style='text-align: centre;'> No more posts to show! </p>";
+		if((strstr($this->user['friend_array'], $usernameComma) || $username_to_check == $this->user['username'])) {
+			return true;
 		}
+		else {
+			return false;
+		}
+	}
 
-		echo $str;
+	public function didReceiveRequest($user_from) {
+		$user_to = $this->user['username'];
+		$check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
+		if(mysqli_num_rows($check_request_query) > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
+	public function didSendRequest($user_to) {
+		$user_from = $this->user['username'];
+		$check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
+		if(mysqli_num_rows($check_request_query) > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public function removeFriend($user_to_remove) {
+		$logged_in_user = $this->user['username'];
+
+		$query = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username='$user_to_remove'");
+		$row = mysqli_fetch_array($query);
+		$friend_array_username = $row['friend_array'];
+
+		$new_friend_array = str_replace($user_to_remove . ",", "", $this->user['friend_array']);
+		$remove_friend = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$logged_in_user'");
+
+		$new_friend_array = str_replace($this->user['username'] . ",", "", $friend_array_username);
+		$remove_friend = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$user_to_remove'");
+	}
+
+	public function sendRequest($user_to) {
+		$user_from = $this->user['username'];
+		$query = mysqli_query($this->con, "INSERT INTO friend_requests (user_to,user_from)VALUES('$user_to', '$user_from')");
+	}
+
+	public function getMutualFriends($user_to_check) {
+		$mutualFriends = 0;
+		$user_array = $this->user['friend_array'];
+		$user_array_explode = explode(",", $user_array);
+
+		$query = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username='$user_to_check'");
+		$row = mysqli_fetch_array($query);
+		$user_to_check_array = $row['friend_array'];
+		$user_to_check_array_explode = explode(",", $user_to_check_array);
+
+		foreach($user_array_explode as $i) {
+
+			foreach($user_to_check_array_explode as $j) {
+
+				if($i == $j && $i != "") {
+					$mutualFriends++;
+				}
+			}
+		}
+		return $mutualFriends;
 
 	}
 
--- config.php
+++ config.php
@@ -11,4 +11,5 @@ if(mysqli_connect_errno())
 	echo "Failed to connect: " . mysqli_connect_errno();
 }
 
+
 ?>
